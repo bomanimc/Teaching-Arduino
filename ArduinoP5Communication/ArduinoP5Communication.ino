e1b8c749e48e95e93fcec97217a34c9f222261d5
Bomani Oseni McClendon <bomanimc@gmail.com>
2020-11-15 10:09:51 -0500

    Add some additonal clarifying code

--- ArduinoP5Communication/ArduinoP5Communication.ino
+++ ArduinoP5Communication/ArduinoP5Communication.ino
@@ -1,11 +1,12 @@
 /*
  * Arduino to P5.js Communication
  * Bomani Oseni McClendon
- *
+ * 
+ * Find the corresponding p5.js code here: https://editor.p5js.org/bomanimc/sketches/QJUjLFUIa
  */
  
 void setup() {
-  // Initialize serial communication.
+  // Initialize serial communication with Baud rate of 9600.
   Serial.begin(9600);
 
   // Set pin A0 as an input.
@@ -14,15 +15,15 @@ void setup() {
  
 void loop() {
   // Read the value from the potentiometer.
-  int potentiometer = analogRead(A0);
+  int potentiometerValue = analogRead(A0);
 
   // Map the potentiometer value so that it fits into one byte.
   // A byte is 8 bits. Remember that 2^8 - 1 = 255.
-  int mappedPot = map(potentiometer, 0, 1023, 0, 255);
+  int mappedPot = map(potentiometerValue, 0, 1023, 0, 255);
   
   // Write the potentiometer value to the serial port.
   Serial.write(mappedPot);
   
   // Add a delay to stabilize the ADC.
-  delay(100);                             
+  delay(100);     
 }
